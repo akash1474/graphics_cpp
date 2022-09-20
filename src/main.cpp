f2802c53a68ed8ac12e0f59e4b8484dddd3aa66a
Akash Pandit <panditakash38@gmail.com>
2022-09-20 07:58:00 +0530

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,88 @@
-#include <chrono>
-#include <graphics.h>
 #include "iostream"
-#include "conio.h"
-#include "thread"
-#include "g_helper.h"
-#include "PerlinNoise.hpp"
-#include "time.h"
+#include "cmath"
+#include "graphics.h" 
 
+
+void bre_line(int x1,int y1,int x2,int y2){
+   if(y2 < y1) {
+      int a=x2,b=y2;
+      x2=x1;
+      y2=y1;
+      x1=a;
+      y1=b;
+   }
+   float dy=abs(y2-y1);
+   float dx=abs(x2-x1);
+   float m=dy/dx;
+   float p=(2*dy)-dx;
+   putpixel(x1, y1, 255);
+   printf("(%d,%d)\n",x1,y1);
+   std::cout << m << std::endl;
+   int n=std::max(dy,dx); 
+   while(n>0){
+      if(p<0){
+         x1+=1;
+         p+=(2*dy);
+         printf("(%d,%d)\n",x1,y1);
+         n--;
+         putpixel(x1, y1, 255);
+      }
+      if(n==0) break;
+      if(p>=0){
+         x1+=1;
+         y1+=1;
+         p+=(2*(dy-dx));
+         printf("(%d,%d)\n",x1,y1);
+         n--;
+         putpixel(x1, y1, 255);
+      }
+   }
+}
+
+
+void dda_line(int x1,int y1,int x2,int y2){
+   float dy=y2-y1;
+   float dx=x2-x1;
+   float m=dy/dx;
+   if(y2 < y1){
+      int a=x2,b=y2;
+      x2=x1;
+      y2=y1;
+      x1=a;
+      y1=b;
+   }
+   std::cout << m << std::endl;
+   int n=std::max(abs(dy),abs(dx));
+   printf("(%d,%d)\n",x1,y1);
+   putpixel(x1,y1, 255);
+   float delta=m>1? (1.0/m) : m;
+   float sum=m>1? delta+x1 : delta+y1 ;
+   while(n>0){
+      if(m<1){
+         x1+=1;
+         y1=round(sum);
+         sum+=delta; 
+      }else{
+         y1+=1;
+         x1=round(sum);
+         sum+=delta;
+      }
+      printf("(%d,%d)\n",x1,y1);
+      putpixel(x1,y1, 255);
+      n--;
+   }
+}
 
 int main()
 {
-   int WIDTH=400;
-   int HEIGHT=400;
-   int FPS=60;
-   int win1=initwindow(WIDTH, HEIGHT,"Perlin Noise",200,200);
-   setcurrentwindow(win1);
-   int n=20;
-   int w=(WIDTH/n),h=HEIGHT/n;
-   Vec2 grid[n][n];
+   int gd=DETECT,gm;
+   initgraph(&gd, &gm,NULL);
    int lineColor=150;
    setcolor(COLOR(lineColor,lineColor,lineColor));
    setbkcolor(COLOR(30,30,30));
 
-   siv::PerlinNoise noise;
-   float inc=0.01;
-   float start=0.0;
-
-   while(true){
-      float xoff=start;
-      if(ismouseclick(WM_LBUTTONDOWN)){
-         inc-=0.01;
-      }
-      if(ismouseclick(WM_RBUTTONDOWN)){
-         inc+=0.01;
-      }
-      clearmouseclick(WM_LBUTTONDOWN);
-      clearmouseclick(WM_RBUTTONDOWN);
-      Vec2 prev;
-      bool isFirst=true;
-      for (int x = 0; x < 50; x++){
-         double ny=noise.octave1D_01(xoff,4);
-         if(isFirst){
-            putpixel(x,ny*HEIGHT,WHITE);
-            isFirst=false;
-         }else{
-            float tx=x*8;
-            float ty=ny*HEIGHT;
-            line(prev.x,prev.y,tx,ty);
-            setcolor(COLOR(108,196,161));
-            line(tx,ty,tx,HEIGHT);
-            setcolor(WHITE);
-            // line(prev.x,prev.y+10,x*8,ny*HEIGHT);
-         }
-         prev.x=x*8;
-         prev.y=ny*HEIGHT;
-         xoff+=inc;
-      }
-      start+=inc;
-      std::this_thread::sleep_for(std::chrono::milliseconds(1000/FPS));
-      cleardevice();
-   }
+   dda_line(200,20,400,100);
+   // dda_line(100,10,25,32);
    std::cout << "Done" << std::endl;
 
    getch();
